1dd78e208f954978d6fc8d7c8a2f51c67bc21491
Matt <mam361@zips.uakron.edu>
2017-09-25 16:54:09 -0400

    Layed out basic structure for syntactic equality

--- syntactic_equality.cpp
+++ syntactic_equality.cpp
@@ -7,7 +7,23 @@
 */
 bool neq(NumExpr e1, NumExpr e2) {
 
-    
+  string n1, n2;
+  n1 = get_node_name(e1);
+  n2 = get_node_name(e2);
+  if(n1 == n2){
+    switch(n1){
+      case "int":
+        break;
+      case "arg":
+        break;
+      case "arith":
+        break;
+      case "if":
+        break;
+     }
+    }
+  }
+  return false;
 
 }
 
@@ -18,5 +34,21 @@ bool neq(NumExpr e1, NumExpr e2) {
   Returns true if they are, and false otherwise.
 */
 bool beq(BoolExpr e1, BoolExpr e2) {
+  
+  string n1, n2;
+  n1 = get_node_name(e1);
+  n2 = get_node_name(e2);
+  if(n1 == n2){
+    switch(n1){
+      case "bool":
+        break;
+      case "rel":
+        break;
+      case "logic":
+        break;
+     }
+    }
+  }
+  return false;
 
 }
