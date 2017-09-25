2941a0364633de49408258041402367731c6e306
Maria <mariakalikas@gmail.com>
2017-09-25 15:00:00 -0400

    added syntactic_equality.cpp file

---
+++ syntactic_equality.cpp
@@ -0,0 +1,19 @@
+/*
+  This function takes two numeric expressions and determines if they are 
+  syntactically the same.
+  Returns true if they are, and false otherwise.
+*/
+bool neq(NumExpr e1, NumExpr e2) {
+
+
+}
+
+
+/*
+  This function takes two boolean expressions and determines if they are 
+  syntactically the same.
+  Returns true if they are, and false otherwise.
+*/
+bool beq(BoolExpr e1, BoolExpr e2) {
+
+}
