b48ff6a92a863dab93c46f190c3f55f055670e0e
Matt <mam361@zips.uakron.edu>
2017-09-27 14:46:09 -0400

    Fixed issue where string was referenced instead of pointers

--- syntactic_equality.cpp
+++ syntactic_equality.cpp
@@ -13,20 +13,20 @@ bool neq(NumExpr e1, NumExpr e2) {
   if(n1 == n2){
     switch(n1){
       case "int":
-        if(n1->val == n2->val){
+        if(e1->val == e2->val){
           return true;
         }
         break;
       case "arg":
-        if(n1->ix == n2->ix){
+        if(e1->ix == e2->ix){
           return true;
         }
         break;
       case "arith":
-        if(n1->op == n2->op){
+        if(e1->op == e2->op){
           //recursively check if the num_exprs are equal
-          if(neq(n1->e1, n2->e2)){
-            if(neq(n1->e2, n2->e2)){
+          if(neq(e1->e1, e2->e2)){
+            if(neq(e1->e2, e2->e2)){
               return true;
             }
           }
@@ -35,9 +35,9 @@ bool neq(NumExpr e1, NumExpr e2) {
       case "if":
         //call beq for the bool expressions of n1 and n2
         //recursively neq for e1 and e2 of n1 and n2
-        if(beq(n1->test,n2->test)){
-          if(neq(n1->e1, n2->e2)){
-            if(neq(n1->e2, n2->e2)){
+        if(beq(e1->test,e2->test)){
+          if(neq(e1->e1, e2->e2)){
+            if(neq(e1->e2, e2->e2)){
               return true;
             }
           }
@@ -64,25 +64,25 @@ bool beq(BoolExpr e1, BoolExpr e2) {
   if(n1 == n2){
     switch(n1){
       case "bool":
-        if(n1->val == n2->val){
+        if(e1->val == e2->val){
           return true;
         }
         break;
       case "rel":
-        if(n1->op == n2->op){
+        if(e1->op == e2->op){
           //recursively check if the bool_exprs are equal
-          if(beq(n1->e1, n2->e2)){
-            if(beq(n1->e2, n2->e2)){
+          if(beq(e1->e1, e2->e2)){
+            if(beq(e1->e2, e2->e2)){
               return true;
             }
           }
         }
         break;
       case "logic":
-        if(n1->op == n2->op){
+        if(e1->op == e2->op){
           //recursively check if the bool_exprs are equal
-          if(beq(n1->e1, n2->e2)){
-            if(beq(n1->e2, n2->e2)){
+          if(beq(e1->e1, e2->e2)){
+            if(beq(e1->e2, e2->e2)){
               return true;
             }
           }
