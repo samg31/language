9c21f96a09f6752ba5babbb76a894d0300729a04
KerryHolmes <kjh80@zips.uakron.edu>
2017-09-29 14:44:45 -0400

    Finish nfold

--- constant_folding.cpp
+++ constant_folding.cpp
@@ -17,32 +17,35 @@ namespace dcipl
             {
                 case ek_int:
                     return static_cast<int_lit*>(num);
-                case k_argc:
+                case ek_argc:
                     return static_cast<arg_expr*>(num);
-                case k_arith:
+                case ek_arith:
                     return eval_arith(static_cast<arith_expr*>(num));
-                case k_if:
+                case ek_if:
                     return eval_if(static_cast<if_expr*>(num));
             }
         }
 
-    }
+        num_expr* 
+        eval_arith(arith_expr* arith)
+        {
+            arith->e1 = nfold(arith->e1);
+            arith->e2 = nfold(arith->e2);
+            if(arith->e1->kind == k_argc || arith->e2->kind == k_argc )
+                return arith;
+            return int_lit(arith->locs, eval_arith_expr(arith));
+        }
 
-    num_expr* 
-    eval_arith(arith_expr* arith)
-    {
-        arith->e1 = nfold(arith->e1);
-        arith->e2 = nfold(arith->e2);
-        if(arith->e1->kind == k_argc || arith->e2->kind == k_argc )
-            return arith;
-        return int_lit(arith->locs, eval_arith_expr(arith));
-    }
+        num_expr* 
+        eval_if(if_expr* cond)
+        {
+            cond->test = bfold(cond->test);
+            cond->e1 = nfold(cond->e1);
+            cond->e2 = nfold(cond->e2);
 
-    num_expr* 
-    eval_if(if_expr* cond)
-    {
-        if(cond->e1->kind == k_argc || cond->e2->kind == k_argc )
-            return cond;
-        return int_lit(cond->locs, eval_if_expr(cond));
+            if(cond->e1->kind == k_argc || cond->e2->kind == k_argc || cond->test->e1->kind == k_argc || cond->test->e2->kind == k_argc)
+                return cond;
+            return int_lit(cond->locs, eval_if_expr(cond));
+        }
     }
 }
